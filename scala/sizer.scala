03415b5e667b212dab0af4fda70ec28d01afd153
Rico Schiekel <fire@downgra.de>
2011-04-30 14:58:36 +0200

    scala: day 3, do 1 and do 2

--- scala/sizer.scala
+++ scala/sizer.scala
@@ -1,11 +1,32 @@
+import java.net.URL
+
 import scala.io._
 import scala.actors._
 import Actor._
+import scala.util.matching.Regex
 
-sealed case class Message(url: String, pageSize: Int)
+case class PageSize(url: String, pageSize: Int)
+case class PageLinkCount(url: String, links: Int)
+case class PageLinks(url: String, links: Seq[String])
+
+case class PageData(pageSize: Option[Int], links: Option[Int])
 
 object PageLoader {
-  def getPageSize(url: String) = Source.fromURL(url, "iso-8859-1").mkString.length
+  val RE_URL = new Regex("""<a .*?href\s*=\s*\"([^\"]*)\"""", "url")
+
+  def readURL(url: String): String = try {
+    Source.fromURL(url, "iso-8859-1").mkString
+  } catch {
+    case e: Exception => ""
+  }
+  def getPageSize(url: String): Int = readURL(url).length
+  def getLinkCount(url: String): Int = getLinks(url).length
+  def getLinks(url: String): Seq[String] = RE_URL.findAllIn(readURL(url))
+                                                 .matchData
+                                                 .map(_.group("url"))
+                                                 .toSeq
+                                                 .filter(_.startsWith("http://"))
+                                                 .distinct
 }
 
 val urls = List( "http://www.amazon.com/"
@@ -22,23 +43,63 @@ def timeMethod(method: () => Unit) = {
 }
 
 def getPageSizeSeq() = {
-  for(url <- urls) {
-    println("Size for " + url + ": " + PageLoader.getPageSize(url))
+  def showURLs(urls: Seq[String]) = {
+    var foundURLs: List[String] = Nil
+    for(url <- urls) {
+      println("  Size for %s: %s [%s Links]".format(url, PageLoader.getPageSize(url), PageLoader.getLinkCount(url)))
+      foundURLs ++= PageLoader.getLinks(url)
+    } 
+    foundURLs.distinct
   }
+  showURLs(showURLs(urls))
 }
 
 def getPageSizeConc() = {
   val caller = self
 
-  for(url <- urls) {
-    actor { caller ! Message(url, PageLoader.getPageSize(url)) }
+  def showURLs(urls: Seq[String]) = {
+    for(url <- urls) {
+      actor {
+        caller ! PageSize(url, PageLoader.getPageSize(url))
+        caller ! PageLinkCount(url, PageLoader.getLinkCount(url))
+        caller ! PageLinks(url, PageLoader.getLinks(url))
+      }
+    }
+
+    var foundURLs: List[String] = Nil
+    var urlData: Map[String, PageData] = Map().withDefaultValue(PageData(None, None))
+
+    def addPageSize(url: String, pageSize: Int) = {
+      urlData = urlData.updated(url, urlData(url) match {
+        case PageData(_, x) => PageData(Some(pageSize), x)
+      })
+      urlData(url)
+    }
+    def addLinkCount(url: String, linkCount: Int) = {
+      urlData = urlData.updated(url, urlData(url) match {
+        case PageData(x, _) => PageData(x, Some(linkCount))
+      })
+      urlData(url)
+    }
+    def showURL(url: String, data: PageData) = data match {
+      case PageData(Some(pageSize), Some(linkCount)) =>
+        println("  Size for %s: %s [%s Links]".format(url, pageSize, linkCount))
+      case _ =>
+    }
+
+    for(i <- 1 to urls.size * 3) {
+      receive {
+        case PageSize(url, size) => showURL(url, addPageSize(url, size))
+        case PageLinkCount(url, links) => showURL(url, addLinkCount(url, links))
+        case PageLinks(url, links) =>
+          foundURLs ++= links
+      }
+    }
+
+    foundURLs.distinct
   }
 
-  for(i <- 1 to urls.size) {
-    receive {
-      case Message(url, size) => println("Size for " + url + ": " + size)
-    }
-  }
+  showURLs(showURLs(urls))
 }
 
 println("Sequential run: ")
