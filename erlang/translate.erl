16fbd763497aebf4014c6729ec06625c7d45ec50
Rico Schiekel <fire@downgra.de>
2011-05-03 20:10:39 +0200

    erlang: day 3, do 1

--- erlang/translate.erl
+++ erlang/translate.erl
@@ -1,5 +1,6 @@
 -module(translate).
 -export([printing_loop/0, responder_loop/0, translate/2]).
+-export([start_translator/0, kill_translator/0, translate/1, translator_restarter/0]).
 
 printing_loop() ->
     receive
@@ -22,6 +23,8 @@ responder_loop() ->
         {Pid, "blanca"} ->
             Pid ! "white",
             responder_loop();
+        diediedie ->
+            exit({"i'm dead now"});
         {Pid, _} ->
             Pid! "what?",
             responder_loop()
@@ -31,4 +34,34 @@ translate(To, Word) ->
     To ! {self(), Word},
     receive
         Translation -> Translation
+    end.
+
+
+
+start_translator() ->
+    spawn(?MODULE, translator_restarter, []).
+
+kill_translator() ->
+    translator ! diediedie.
+
+translate(Word) ->
+    translator !{self(), Word},
+    receive
+        Translation -> Translation
+    end.
+
+translator_restarter() ->
+    process_flag(trap_exit, true),
+    Pid = spawn_link(?MODULE, responder_loop, []),
+    case lists:member(translator, registered()) of
+        true -> unregister(translator);
+        _ -> ok
+    end,
+    register(translator, Pid),
+    receive
+        {'EXIT', Pid, normal} -> ok;
+        {'EXIT', Pid, shutdown} -> ok;
+        {'EXIT', Pid, _} ->
+            io:format("restarting translator service...~n"),
+            translator_restarter()
     end.
