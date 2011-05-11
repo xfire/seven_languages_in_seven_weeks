9b7b3d6c910810f8b11f43c4874f24cbba4d388d
Rico Schiekel <fire@downgra.de>
2011-05-11 21:02:55 +0200

    haskell: day 2, do 2

--- haskell/sort.hs
+++ haskell/sort.hs
@@ -1,5 +1,16 @@
+import Data.List
+
 qsort :: Ord a => [a] -> [a]
 qsort [] = []
 qsort (x:xs) = (qsort lower) ++ [x] ++ (qsort upper)
     where lower = filter (< x) xs
           upper = filter (>= x) xs
+
+-- qsortBy (\a b -> if (a < b) then LT else GT) [44,2,12,51,9,31,25]
+qsortBy :: (a -> a -> Ordering) -> [a] -> [a]
+qsortBy _ [] = []
+qsortBy f (x:xs) = (qsortBy f lower) ++ [x] ++ (qsortBy f upper)
+    where lower = filter lt xs
+          upper = filter gt xs
+          lt a = (f a x) == LT
+          gt a = (f a x) == GT
