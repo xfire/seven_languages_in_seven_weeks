dd8816b431768bda13208387da94c5bc14db3997
Rico Schiekel <fire@downgra.de>
2011-05-11 22:24:26 +0200

    haskell: day 2, bonus 4

--- haskell/breakString.hs
+++ haskell/breakString.hs
@@ -1,3 +1,5 @@
 breakString :: Int -> String -> [String]
 breakString n xs = map unwords $ (chunks n) $ words xs
     where chunks n = takeWhile (not . null) . map (take n) . iterate (drop n)
+
+breakStringLN n xs = zipWith (\a b -> (show a) ++ ": " ++ b) [1..] (breakString n xs)
